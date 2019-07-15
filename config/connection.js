c637f4d1a68d0adc31382692ea9bf978aed84410
Felix <46941065+felix911104@users.noreply.github.com>
2019-07-15 08:50:44 -0700

    update1

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+// Create the MySQL connection object
+var connection;
+
+
+	// DB is local on localhost
+connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '26400918a',
+		database: 'burgers_db'
+})
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+// Export connection for ORM use
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vaggie Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
